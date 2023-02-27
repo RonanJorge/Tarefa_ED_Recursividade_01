fd5441df361e2653c4590ef45b73cc2541335790
Ronan Jorge <Ronan Jorge@10.0.0.108>
2023-02-26 22:59:16 -0300

    Atualização 01

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,3 +1,10 @@
+/*	Nome: Ronan Jorge
+ * 	Data: 26/02/2023
+ * 	Objetivo: 01) Crie uma função recursiva que exiba o resultado do fatorial de um
+	número (Pela limitação da recursividade, o número de entrada deverá
+	ser baixo para não dar estouro(limite de entrada = 12)):
+ */
+
 package view;
 
 import javax.swing.JOptionPane;
